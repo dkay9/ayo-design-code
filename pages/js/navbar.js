34d0e68ed7448ccca1436c8afd9bb3780420de1c
Kalu David <kaludavid411@gmail.com>
2025-01-05 16:43:37 +0100

    new push

--- pages/js/navbar.js
+++ pages/js/navbar.js
@@ -6,6 +6,15 @@ let links = document.getElementById("links");
 hamburger.addEventListener("click", hideLinks); // When hamburger is clicked, the links appear or disappear
 hamburger.addEventListener("click", changeMenuState); // When hamburger is clicked, it changes into menu or x
 
+// When one of the links is clicked, the links disappear
+let link = document.querySelectorAll("div#links > ul > *");
+for (let i = 0; i < link.length; i++) {
+	let allLinks = link[i];
+	allLinks.addEventListener("click", hideLinks);
+	allLinks.addEventListener("click", removeActive);
+	allLinks.addEventListener("click", removeScroll);
+}
+
 // When the outer part of the hamburger menu is clicked the links disappear
 main.addEventListener("click", hideLinksOnly);
 main.addEventListener("click", removeActive);
